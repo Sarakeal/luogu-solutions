a07e30c01f84252dd280582d5104a8acdfdc6956
sliu <sliu@pisx.com>
2020-11-25 14:51:05 +0800

    增加src目录

--- src/P1008.cpp
+++ src/P1008.cpp
@@ -13,12 +13,15 @@ int main()
 		int count = 0;
 		memset(arr, 0, sizeof(arr));
 		arr[num % 10] = arr[num / 10 % 10] = arr[num / 100] = arr[2 * num % 10] = arr[2 * num / 10 % 10] = arr[2 * num / 100] = arr[3 * num % 10] = arr[3 * num / 10 % 10] = arr[3 * num / 100] = 1;
-		for (int i = 1; i <= 9; i++) {
-			if (arr[i] == 1) {
+		for (int i = 1; i <= 9; i++)
+		{
+			if (arr[i] == 1)
+			{
 				count++;
 			}
 		}
-		if (count == 9) {
+		if (count == 9)
+		{
 			cout << num << " " << 2 * num << " " << 3 * num << endl;
 		}
 	}
